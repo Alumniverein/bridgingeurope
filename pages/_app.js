a10ab5202283fa09c5c7f06561916c06e9f70518
jkamphausen <juliankamphausen@me.com>
2020-10-22 06:58:23 +0200

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
 import '../styles/globals.css'
+import * as gtag from '../lib/gtag'
+import Router from 'next/router'
+
+// Notice how we track pageview when route is changed
+Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,4 @@
+import { Fragment } from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components';
 class MyDocument extends Document {
@@ -5,13 +6,44 @@ class MyDocument extends Document {
         const sheet = new ServerStyleSheet();
         const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
         const styleTags = sheet.getStyleElement();
-        return { ...page, styleTags };
+
+        // Check if in production
+        const isProduction = process.env.NODE_ENV === 'production'
+
+        return { ...page, styleTags, isProduction };
     }
 
     render() {
+        const { isProduction } = this.props
+
         return (
             <Html lang="de">
-                <Head>{this.props.styleTags}</Head>
+                <Head>
+                    {this.props.styleTags}
+                    {/* We only want to add the scripts if in production */}
+                    {isProduction && (
+                        <Fragment>
+                            {/* Global Site Tag (gtag.js) - Google Analytics */}
+                            <script
+                                async
+                                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                            />
+                            <script
+                                dangerouslySetInnerHTML={{
+                                    __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', '${GA_TRACKING_ID}', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+                                }}
+                            />
+                        </Fragment>
+                    )}
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
