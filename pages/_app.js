8bc4981b3175c0b565ed04920b9bda98b79e29e0
jkamphausen <juliankamphausen@me.com>
2020-10-22 07:43:07 +0200

    rollback

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,4 @@
 import '../styles/globals.css'
-import * as gtag from '../lib/gtag'
-import Router from 'next/router'
-
-// Notice how we track pageview when route is changed
-Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
